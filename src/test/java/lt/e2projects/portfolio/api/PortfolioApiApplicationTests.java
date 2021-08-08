a7b349d71e3632aaa96c1b49e9ddd0fcdeb3c726
Dum6o <edwardasas@gmail.com>
2021-08-08 09:23:10 +0300

    Create portfolio-api

---
+++ src/test/java/lt/e2projects/portfolio/api/PortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.e2projects.portfolio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
